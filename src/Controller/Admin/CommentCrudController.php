624c81d68d805657777e38a69542b2457ca88165
Sławek Król <slawekkrol@MacBook-Pro-Sawek.local>
2023-03-11 14:11:14 +0100

    Added a few features - Added admin panel (EasyAdmin) - Configured CRUD for Conferences - Configured CRUD for Comments - Configured filters for Comments - Buliding Conferences list - Buildind conference details with comments - Implemented pagination for comments in conference details page

---
+++ src/Controller/Admin/CommentCrudController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+
+class CommentCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Comment::class;
+    }
+
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Conference Comment')
+            ->setEntityLabelInPlural('Conference Comments')
+            ->setSearchFields(['author', 'text', 'email'])
+            ->setDefaultSort(['createdAt' => 'DESC']);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        // definiujemy jakie pola widoczne w wyszukiwarce
+        return $filters
+            ->add('email')
+            ->add(EntityFilter::new('conference'));
+    }
+
+    /**
+     *
+     * Konfiguracja pól
+     * kolejnoś, czy maja byc widoczne
+     * @param string $pageName
+     * @return iterable
+     */
+    public function configureFields(string $pageName): iterable
+    {
+        yield AssociationField::new('conference');
+        yield TextField::new('author');
+        yield EmailField::new('email');
+        yield TextareaField::new('text')
+            ->hideOnIndex(); // ukywamy na liscie
+
+        yield TextField::new('photoFilename')
+            ->onlyOnIndex(); // pokazujemy tylko na liscie
+
+        $createdAt = DateTimeField::new('createdAt')->setFormTypeOptions([
+            'html5' => true,
+            'years' => range(date('Y'), date('Y') + 5),
+            'widget' => 'single_text',
+        ]);
+
+        if (Crud::PAGE_EDIT === $pageName) {
+            yield $createdAt->setFormTypeOption('disabled', true);
+        } else {
+            yield $createdAt;
+        }
+
+    }
+
+}
